0d91a6aadc30650e547592885f00101345d869a7
Dan Mayer <dan@devver.net>
2009-06-28 17:43:19 -0600

    test verifies that it is running on postgres DB not converted to and using mysql

--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -5,4 +5,9 @@ class PersonTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+
+  test "that we are testing on postgres" do
+    assert_equal "PostgreSQL", ActiveRecord::Base.connection.adapter_name
+  end
+
 end
